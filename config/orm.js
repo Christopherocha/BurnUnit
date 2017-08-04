eebee5f561a2651343b4810576cc881c6dfad905
Chris R <chris.rocha@>
2017-08-04 17:36:54 -0500

    getting firebase configs in there

---
+++ config/orm.js
@@ -0,0 +1,12 @@
+var fireBase = require('firebase');
+var config = ('./fbConfig');
+
+//  Initialize the firebase app
+var app = firebase.initializeApp(config);
+
+var firebaseQueries = {
+    
+}
+
+
+
