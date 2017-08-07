3baab13151d5ff6ede781166b4be4200752ae7a8
kaylea <kayleabox09@gmail.com>
2017-08-06 20:14:46 -0500

    created public js files for some of the pages. need to start working on the logic for them

---
+++ public/assets/js/login.js
@@ -0,0 +1,10 @@
+var userID; 
+
+//when login submit button is clicked check to see that username and password are entered
+//if user entered username and password, check to see if they are in the data base
+
+//if they are in the database, get the UserId and set to local var userId
+//redirect to profile or game page with url param id set to userId
+
+//if they are not in the database, show message that their was an error with 
+//username and password verification
---
+++ public/assets/js/profile.js
@@ -0,0 +1,11 @@
+var userId;
+
+//query user table for current user's information this can be done through routes
+//this the route should res.json the profile page with the user object
+
+
+//based on userId query roast table to see how many roasts the user has won
+//if they have won a roast save the QuoteID 
+//get the number of roasts that the user has initiated
+
+//use QuoteId from roasts to query the quote table and get the winnig quotes
